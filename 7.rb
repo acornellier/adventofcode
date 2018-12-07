e25a1b8e8de11238ff1aca9c8f2ea65c0ba4c968
Alex Cornellier <alex@cornellier.com>
2018-12-07 00:36:02 -0500

    7

---
+++ 7.rb
@@ -0,0 +1,49 @@
+lines = File.readlines('input.txt').map(&:strip)
+
+steps = lines.map do |line|
+  [line[5], line[36]]
+end
+
+letters = steps.flatten.uniq
+
+h1 = letters.each_with_object({}) do |letter, h|
+  h[letter] = []
+end
+
+steps.each do |step|
+  h1[step[1]] << step[0]
+end
+
+pending = {}
+completed = []
+workers = 5
+seconds = 0
+
+loop do
+  pending.each do |k, v|
+    pending[k] -= 1
+    if pending[k] == 0
+      completed << k
+      pending.delete(k)
+      workers += 1
+    end
+  end
+
+  break if completed.length == letters.length
+
+  potential = []
+  h1.each do |a, b|
+    next if completed.include? a
+    potential << a if (b - completed).empty?
+  end
+
+  potential.each do |step|
+    next if completed.include?(step) || pending[step]
+    break if workers == 0
+    pending[step] = 60 + step.ord - 'A'.ord + 1
+    workers -= 1
+  end
+  seconds += 1
+end
+
+p seconds
