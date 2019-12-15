2796d8714cc484ec20a68ce91780222ca78abde1
alex <alex@cornellier.com>
2019-12-15 12:24:41 -0500

    2019 14

--- 2019/14.rb
+++ 2019/14.rb
@@ -1,42 +1,73 @@
 require_relative 'util'
 
-rules = []
-lines.each do |line|
+rules = lines.to_h do |line|
   a, b = line.split(' => ')
-  aa = a.split(', ').map { |aaa| x, y = aaa.split; [x.to_i, y] }
+  aa = a.split(', ').map { |aaa| x, y = aaa.split; [y, x.to_f] }
   x, y = b.split
-  rules << [aa, [x.to_i, y]]
+  [[y, x.to_f], aa]
 end
 
-max_needed = Hash.new(0)
-rules.each do |k, v|
-  k.each do |n, el|
-    max_needed[el] = n if n > max_needed[el]
-  end
-end
+STARTING_ORE = 1e12
+need = Hash.new(0)
+need['FUEL'] = 1
+have = Hash.new(0)
+have['ORE'] = STARTING_ORE
 
-INITIAL_ORE = 100_000
-initial_state = Hash.new(0)
-initial_state['ORE'] = INITIAL_ORE
+def make_fuel(rules, need, have)
+  until need.empty?
+    need_ele, need_n = need.shift
+    (rule_out_ele, rule_out_n), rule_in = rules.find do |(out_ele, out_n), input|
+      out_ele == need_ele
+    end
 
-exit_condition = ->(state) { state.key?('FUEL') }
-
-neighbors = ->(state) do
-  rules.select do |k, (m, out_el)|
-    state[out_el] <= max_needed[out_el] &&
-      k.all? { |n, in_el| state[in_el] >= n }
-  end.map do |k, (m, out_el)|
-    state.dup.tap do |new|
-      k.each do |n, ele|
-        new[ele] -= n
+    num = (need_n.to_f / rule_out_n).ceil
+    rule_in.each do |in_ele, n|
+      qty = n * num
+      stored = have[in_ele]
+      if qty <= stored
+        have[in_ele] -= qty
+      else
+        have.delete(in_ele)
+        need[in_ele] += qty - stored
       end
-      new[out_el] += m
+    end
+
+    break if have['ORE'] <= 0
+
+    need.delete(need_ele)
+    have[need_ele] += num * rule_out_n - need_n
+    have.select! do |have_ele, have_n|
+      have_n > 0
     end
   end
 end
 
-distance = ->(neighbor, _shortest_distance) do
-  INITIAL_ORE - neighbor['ORE']
+make_fuel(rules, need, have)
+p STARTING_ORE - have['ORE']
+
+need = Hash.new(0)
+have = Hash.new(0)
+have['ORE'] = STARTING_ORE
+fuel = 0
+
+12.downto(0).each do |power|
+  batch = 10 ** power
+
+  loop do
+    need['FUEL'] = batch
+    need_backup = need.dup
+    have_backup = have.dup
+
+    make_fuel(rules, need, have)
+
+    if have['ORE'] > 0
+      fuel += batch
+    else
+      need = need_backup
+      have = have_backup
+      break
+    end
+  end
 end
 
-p dijkstra(initial_state, exit_condition, neighbors, distance)
+p fuel
