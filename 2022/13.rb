410b6c7e660e8b32912bc76ed6b43b1fadd3b4d4
Alex Cornellier <alex@cornellier.com>
2022-12-13 00:59:55 -0500

    improvements from another reddit comment

--- 2022/13.rb
+++ 2022/13.rb
@@ -1,24 +1,20 @@
 def compare(x, y)
   case [x, y]
-  in [NilClass, _]
-    -1
-  in [_, NilClass]
-    1
   in [Integer, Integer]
-    x < y ? -1 : x > y ? 1 : 0
+    x <=> y
   in [Array, Array]
-    (0...[x.size, y.size].max).each do |i|
+    (0...[x.size, y.size].min).each do |i|
       res = compare(x[i], y[i])
       return res if res != 0
     end
-    0
+    x.size <=> y.size
   else
     compare([x].flatten(1), [y].flatten(1))
   end
 end
 
 dividers = [[[2]], [[6]]]
-packets = $<.reject { _1 == ?\n }.map { eval(_1) } + dividers
-packets.sort! { compare(_1, _2) }
+packets = $<.reject { _1 == ?\n }.map(&method(:eval)) + dividers
+packets.sort!(&method(:compare))
 
 p dividers.map { packets.index(_1) + 1 }.inject(:*)
